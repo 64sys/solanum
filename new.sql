5cdf5ac5b6b49d27fe9aa1d79f672f35c2a14c2b
belmotek <planeta4@openmailbox.org>
2017-02-05 02:49:16 +0100

    +Funcion reemplaza letras acentuadas. +Agregar vegetales con un clic a la parcela

--- new.sql
+++ new.sql
@@ -35,16 +35,16 @@ CREATE TABLE `plants` (
 INSERT INTO `plants` VALUES (1,'Berenjena','Berenjena','12',180,180);
 INSERT INTO `plants` VALUES (2,'Lechuga','Lechuga','123456789',90,90);
 INSERT INTO `plants` VALUES (3,'Papa','Patata','1234',120,120);
-INSERT INTO `plants` VALUES (4,'Pimiento','Pimiento','123',150,150);
+INSERT INTO `plants` VALUES (4,'Morrón','Pimiento','123',150,150);
 INSERT INTO `plants` VALUES (5,'Puerro','Puerro','123456',120,150);
 INSERT INTO `plants` VALUES (6,'Tomate','Tomate','1234',150,150);
 INSERT INTO `plants` VALUES (7,'Acelga','Acelga','23456789',0,0);
 INSERT INTO `plants` VALUES (8,'Apio','Apio','2345',240,240);
-INSERT INTO `plants` VALUES (9,'Zapallito','Calabacín','23',90,90);
+INSERT INTO `plants` VALUES (9,'Zucchini','Calabacín','23',90,90);
 INSERT INTO `plants` VALUES (10,'Escarola','Escarola','23456789',90,0);
 INSERT INTO `plants` VALUES (11,'Pepino','Pepino','2345',90,120);
 INSERT INTO `plants` VALUES (12,'Zanahoria','Zanahoria','23456789',90,120);
-INSERT INTO `plants` VALUES (13,'Chaucha','Judías','3456',90,120);
+INSERT INTO `plants` VALUES (13,'Chauchas','Judías','3456',90,120);
 INSERT INTO `plants` VALUES (14,'Melón','Melón','34',120,120);
 INSERT INTO `plants` VALUES (15,'Sandía','Sandía','34',120,120);
 INSERT INTO `plants` VALUES (16,'Repollo','Col','3456789A',150,150);
@@ -54,9 +54,19 @@ INSERT INTO `plants` VALUES (19,'Cebolla','Cebolla','01789AB',270,270);
 INSERT INTO `plants` VALUES (20,'Espinaca','Espinaca','01789AB',90,90);
 INSERT INTO `plants` VALUES (21,'Habas','Habas','89A',120,120);
 INSERT INTO `plants` VALUES (22,'Ajo','Ajo','09AB',270,270);
-INSERT INTO `plants` VALUES (23,'Guisantes','Guisantes','019AB',120,120);
+INSERT INTO `plants` VALUES (23,'Arbejas','Guisantes','019AB',120,120);
 INSERT INTO `plants` VALUES (24,'Rábano','Rábano','0123456789AB',30,60);
 INSERT INTO `plants` VALUES (25,'Zapallo','Calabaza','',0,0);
+INSERT INTO `plants` VALUES (26,'Ají','Ají','',0,0);
+INSERT INTO `plants` VALUES (27,'Alcaucil','Alcaucil','',0,0);
+INSERT INTO `plants` VALUES (28,'Batata','Batata','',0,0);
+INSERT INTO `plants` VALUES (29,'Cebolla-roja','Cebolla-roja','',0,0);
+INSERT INTO `plants` VALUES (30,'Champiñon','Champiñon','',0,0);
+INSERT INTO `plants` VALUES (31,'Choclo','Choclo','',0,0);
+INSERT INTO `plants` VALUES (32,'Esparrago','Esparrago','',0,0);
+INSERT INTO `plants` VALUES (33,'Morrón-amarillo','Morrón-amarillo','123',150,150);
+INSERT INTO `plants` VALUES (34,'Remolacha','Remolacha','',0,0);
+INSERT INTO `plants` VALUES (35,'Repollo-rojo','Col-roja','3456789A',150,150);
 CREATE TABLE `cycles` (
 	`cycleidx`	INTEGER PRIMARY KEY AUTOINCREMENT,
 	`cyclename`	TEXT NOT NULL UNIQUE,
@@ -73,18 +83,18 @@ CREATE TABLE `blocks` (
 	FOREIGN KEY(`blockcycle`) REFERENCES `cycles` ( `cycleidx` ),
 	FOREIGN KEY(`blockplant`) REFERENCES `plants` ( `plantidx` )
 );
-INSERT INTO `blocks` VALUES (1,1,1,NULL,1);
-INSERT INTO `blocks` VALUES (2,1,1,NULL,6);
 CREATE UNIQUE INDEX "zones_idx" on zones (zonename ASC);
 CREATE UNIQUE INDEX "soils_idx" on soils (soilname ASC);
 CREATE UNIQUE INDEX "plots_idx" on plots (plotname ASC);
 CREATE UNIQUE INDEX "plants_idx" on plants (plantname ASC);
 CREATE UNIQUE INDEX "cycles_idx" on cycles (cyclename ASC);
-CREATE VIEW `view_blocks` AS
+CREATE VIEW "view_blocks" AS
 SELECT
 blockidx,
 blockplot AS blockplotidx,
 plotname AS blockplot,
+plotlx AS blockplotx,
+plotly AS blockploty,
 blockcycle AS blockcycleidx,
 cyclename AS blockcycle,
 plotname || '-' || cyclename AS blockplotcycle,
